4f0970d3045d55824c495bf4eada4b7c077ced49
CallumRG <callum.r.gillies@gmail.com>
2024-02-15 16:24:26 -0500

    specify app should list restaurants

---
+++ cypress/e2e/listing-restaurants.cy.js
@@ -0,0 +1,14 @@
+describe('Listing Restaurants', () => {
+    it('shows restaurants from the server', () => {
+        const sushiPlace = 'Sushi Place';
+        const pizzaPlace = 'Pizza Place';
+        cy.intercept('GET', 'https://api.outsidein.dev/tIPk37H5cTrXKGetT8SJL7O6YNdPR0DZ/restaurants', [
+            {id: 1, name: sushiPlace},
+            {id: 2, name: pizzaPlace},
+        ]);
+        cy.visit('/');
+        cy.contains(sushiPlace);
+        cy.contains(pizzaPlace);
+    });
+});
+    
