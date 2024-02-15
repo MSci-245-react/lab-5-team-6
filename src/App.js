7c1c2334e4bca6ffbea11557aaba0647f34563c1
CallumRG <callum.r.gillies@gmail.com>
2024-02-15 17:47:25 -0500

    added redux aspects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import {Provider} from 'react-redux';
+import store from './store';
 import RestaurantScreen from './components/RestaurantScreen';
+
 
 export default function App() {
   return (
-    <div>
-    <RestaurantScreen />
-    </div>
+    <Provider store={store}>
+      <RestaurantScreen />
+    </Provider>
   );
 }
